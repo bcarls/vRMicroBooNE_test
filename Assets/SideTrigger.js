08ab827e0d3d2eb992d1fb00ffdf205d2432cf5d
bcarls <bcarls@mac-120200.dhcp.fnal.gov>
2014-07-10 10:30:32 -0500

    Test commit of the dice Unity project

---
+++ Assets/SideTrigger.js
@@ -0,0 +1,13 @@
+﻿public var faceValue = 0;
+
+function OnTriggerEnter( other : Collider ) {
+
+dieGameObject = GameObject.Find('SixSidedDie');
+
+dieValueComponent = dieGameObject.GetComponent('DieValue');
+
+dieValueComponent.currentValue = faceValue;
+
+Debug.Log(faceValue);
+
+}
